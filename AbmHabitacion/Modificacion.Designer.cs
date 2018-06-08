63cc9b502898bf01b29de20be38f1a0b951976c6
Nicolas Liloia Scott <liloiascott@gmail.com>
2018-06-08 20:20:44 -0300

    modificando habitaciones

--- AbmHabitacion/Modificacion.Designer.cs
+++ AbmHabitacion/Modificacion.Designer.cs
@@ -29,39 +29,39 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBoxHotel = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBoxUbicacion = new System.Windows.Forms.ComboBox();
+            this.textBoxComodidades = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBoxDescripcion = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBoxTipoHabitacion = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxPiso = new System.Windows.Forms.TextBox();
+            this.textBoxNumHabitacion = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.checkBoxHabilitado = new System.Windows.Forms.CheckBox();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.buttonLimpiar = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBoxHotel);
             this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.comboBoxUbicacion);
+            this.groupBox1.Controls.Add(this.textBoxComodidades);
             this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBoxDescripcion);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBoxTipoHabitacion);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBoxPiso);
+            this.groupBox1.Controls.Add(this.textBoxNumHabitacion);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
@@ -73,21 +73,41 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos de la nueva habitación";
             // 
-            // comboBox1
+            // textBoxHotel
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(91, 139);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(214, 28);
-            this.comboBox1.TabIndex = 13;
+            this.textBoxHotel.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxHotel.Enabled = false;
+            this.textBoxHotel.Location = new System.Drawing.Point(59, 37);
+            this.textBoxHotel.MaxLength = 255;
+            this.textBoxHotel.Name = "textBoxHotel";
+            this.textBoxHotel.Size = new System.Drawing.Size(246, 26);
+            this.textBoxHotel.TabIndex = 15;
             // 
-            // textBox6
+            // label7
             // 
-            this.textBox6.Location = new System.Drawing.Point(119, 241);
-            this.textBox6.MaxLength = 255;
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(310, 26);
-            this.textBox6.TabIndex = 12;
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label7.Location = new System.Drawing.Point(6, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Hotel";
+            // 
+            // comboBoxUbicacion
+            // 
+            this.comboBoxUbicacion.FormattingEnabled = true;
+            this.comboBoxUbicacion.Location = new System.Drawing.Point(91, 139);
+            this.comboBoxUbicacion.Name = "comboBoxUbicacion";
+            this.comboBoxUbicacion.Size = new System.Drawing.Size(214, 28);
+            this.comboBoxUbicacion.TabIndex = 13;
+            // 
+            // textBoxComodidades
+            // 
+            this.textBoxComodidades.Location = new System.Drawing.Point(119, 241);
+            this.textBoxComodidades.MaxLength = 255;
+            this.textBoxComodidades.Name = "textBoxComodidades";
+            this.textBoxComodidades.Size = new System.Drawing.Size(310, 26);
+            this.textBoxComodidades.TabIndex = 12;
             // 
             // label6
             // 
@@ -99,13 +119,13 @@
             this.label6.TabIndex = 11;
             this.label6.Text = "Comodidades";
             // 
-            // textBox5
+            // textBoxDescripcion
             // 
-            this.textBox5.Location = new System.Drawing.Point(104, 207);
-            this.textBox5.MaxLength = 255;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(325, 26);
-            this.textBox5.TabIndex = 10;
+            this.textBoxDescripcion.Location = new System.Drawing.Point(104, 207);
+            this.textBoxDescripcion.MaxLength = 255;
+            this.textBoxDescripcion.Name = "textBoxDescripcion";
+            this.textBoxDescripcion.Size = new System.Drawing.Size(325, 26);
+            this.textBoxDescripcion.TabIndex = 10;
             // 
             // label5
             // 
@@ -117,15 +137,15 @@
             this.label5.TabIndex = 9;
             this.label5.Text = "Descripción";
             // 
-            // textBox4
+            // textBoxTipoHabitacion
             // 
-            this.textBox4.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox4.Enabled = false;
-            this.textBox4.Location = new System.Drawing.Point(153, 173);
-            this.textBox4.MaxLength = 18;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(152, 26);
-            this.textBox4.TabIndex = 8;
+            this.textBoxTipoHabitacion.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxTipoHabitacion.Enabled = false;
+            this.textBoxTipoHabitacion.Location = new System.Drawing.Point(153, 173);
+            this.textBoxTipoHabitacion.MaxLength = 18;
+            this.textBoxTipoHabitacion.Name = "textBoxTipoHabitacion";
+            this.textBoxTipoHabitacion.Size = new System.Drawing.Size(152, 26);
+            this.textBoxTipoHabitacion.TabIndex = 8;
             // 
             // label4
             // 
@@ -137,21 +157,21 @@
             this.label4.TabIndex = 7;
             this.label4.Text = "Tipo de Habitación";
             // 
-            // textBox2
+            // textBoxPiso
             // 
-            this.textBox2.Location = new System.Drawing.Point(51, 105);
-            this.textBox2.MaxLength = 18;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(129, 26);
-            this.textBox2.TabIndex = 4;
+            this.textBoxPiso.Location = new System.Drawing.Point(51, 105);
+            this.textBoxPiso.MaxLength = 18;
+            this.textBoxPiso.Name = "textBoxPiso";
+            this.textBoxPiso.Size = new System.Drawing.Size(129, 26);
+            this.textBoxPiso.TabIndex = 4;
             // 
-            // textBox1
+            // textBoxNumHabitacion
             // 
-            this.textBox1.Location = new System.Drawing.Point(176, 71);
-            this.textBox1.MaxLength = 18;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(129, 26);
-            this.textBox1.TabIndex = 3;
+            this.textBoxNumHabitacion.Location = new System.Drawing.Point(176, 71);
+            this.textBoxNumHabitacion.MaxLength = 18;
+            this.textBoxNumHabitacion.Name = "textBoxNumHabitacion";
+            this.textBoxNumHabitacion.Size = new System.Drawing.Size(129, 26);
+            this.textBoxNumHabitacion.TabIndex = 3;
             // 
             // label3
             // 
@@ -183,64 +203,44 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Número de habitación";
             // 
-            // checkBox1
+            // checkBoxHabilitado
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Checked = true;
-            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Indeterminate;
-            this.checkBox1.Location = new System.Drawing.Point(22, 320);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(106, 24);
-            this.checkBox1.TabIndex = 26;
-            this.checkBox1.Text = "Habilitado";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBoxHabilitado.AutoSize = true;
+            this.checkBoxHabilitado.Checked = true;
+            this.checkBoxHabilitado.CheckState = System.Windows.Forms.CheckState.Indeterminate;
+            this.checkBoxHabilitado.Location = new System.Drawing.Point(22, 320);
+            this.checkBoxHabilitado.Name = "checkBoxHabilitado";
+            this.checkBoxHabilitado.Size = new System.Drawing.Size(106, 24);
+            this.checkBoxHabilitado.TabIndex = 26;
+            this.checkBoxHabilitado.Text = "Habilitado";
+            this.checkBoxHabilitado.UseVisualStyleBackColor = true;
             // 
-            // label7
+            // buttonGuardar
             // 
-            this.label7.AutoSize = true;
-            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label7.Location = new System.Drawing.Point(6, 40);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(47, 20);
-            this.label7.TabIndex = 14;
-            this.label7.Text = "Hotel";
+            this.buttonGuardar.Location = new System.Drawing.Point(471, 377);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(84, 28);
+            this.buttonGuardar.TabIndex = 28;
+            this.buttonGuardar.Text = "Guardar";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
             // 
-            // textBox3
+            // buttonLimpiar
             // 
-            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox3.Enabled = false;
-            this.textBox3.Location = new System.Drawing.Point(59, 37);
-            this.textBox3.MaxLength = 255;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(246, 26);
-            this.textBox3.TabIndex = 15;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(471, 377);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(84, 28);
-            this.button2.TabIndex = 28;
-            this.button2.Text = "Guardar";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(23, 377);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 28);
-            this.button1.TabIndex = 27;
-            this.button1.Text = "Limpiar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonLimpiar.Location = new System.Drawing.Point(23, 377);
+            this.buttonLimpiar.Name = "buttonLimpiar";
+            this.buttonLimpiar.Size = new System.Drawing.Size(83, 28);
+            this.buttonLimpiar.TabIndex = 27;
+            this.buttonLimpiar.Text = "Limpiar";
+            this.buttonLimpiar.UseVisualStyleBackColor = true;
             // 
             // Modificacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(578, 424);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.buttonGuardar);
+            this.Controls.Add(this.buttonLimpiar);
+            this.Controls.Add(this.checkBoxHabilitado);
             this.Controls.Add(this.groupBox1);
             this.Name = "Modificacion";
             this.Text = "Modificacion";
@@ -254,22 +254,22 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox comboBoxUbicacion;
+        private System.Windows.Forms.TextBox textBoxComodidades;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBoxDescripcion;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBoxTipoHabitacion;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxPiso;
+        private System.Windows.Forms.TextBox textBoxNumHabitacion;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBoxHotel;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBoxHabilitado;
+        private System.Windows.Forms.Button buttonGuardar;
+        private System.Windows.Forms.Button buttonLimpiar;
     }
 }
