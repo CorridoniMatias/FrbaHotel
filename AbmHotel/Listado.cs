9716cacd5513e0b75ab6deea753638993b0d81ef
Matias Mendez Corridoni <matim97@gmail.com>
2018-06-08 14:47:28 -0300

    Join agregado para #11

--- AbmHotel/Listado.cs
+++ AbmHotel/Listado.cs
@@ -19,32 +19,42 @@ namespace FrbaHotel.AbmHotel
 
         private void Listado_Load(object sender, EventArgs e)
         {
+            this.DialogResult = System.Windows.Forms.DialogResult.Abort;
             this.Poblar();
         }
 
         private void Poblar()
         {
-            var filtro = new QueryBuilder(QueryBuilder.QueryBuilderType.SELECT).
-                Fields("idHotel, nombre, cantidadEstrellas,telefono,mail,ciudad,pais").Table("MATOTA.Hotel");
+            var filtro = new QueryBuilder(QueryBuilder.QueryBuilderType.SELECT)
+                .Fields("h.idHotel, h.nombre, h.cantidadEstrellas,h.telefono,h.mail,h.ciudad,h.pais")
+                .Table("MATOTA.Hotel h")
+                .AddJoin("INNER JOIN MATOTA.HotelesUsuario hu ON h.idHotel = hu.idHotel")
+                .AddEquals("hu.idUsuario", Login.Login.LoggedUsedID.ToString());
 
             new List<TextBox>() { textBoxCantEstrellas, textBoxCiudad, textBoxNombre, textBoxPais, textBoxMail, textBoxTelefono }
                 .FindAll(c => !string.IsNullOrEmpty(c.Text.Trim()))
                 .ForEach(c =>
                     filtro.AddEquals(c.Tag.ToString(), c.Text)
                 );
-
+            
             try
             {
                 var newset = DBHandler.Query(filtro.Build()).Select(row =>
                     new List<string>() { row["idHotel"].ToString(), row["nombre"].ToString(), row["cantidadEstrellas"].ToString(), row["telefono"].ToString(), row["mail"].ToString(), row["ciudad"].ToString(), row["pais"].ToString(), "Seleccionar" }
                 ).ToList();
 
+                if (newset.Count() == 0)
+                {
+                    MessageBox.Show("No se encontró ningún hotel donde esté vinculado. Intente cambiar el criterio de búsqueda.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
 
                 newset.ForEach(row =>
                         dataGridView1.Rows.Add(row.ToArray())
                     );
             }
-            catch (Exception)
+            catch (Exception e)
             {
                 this.DialogResult = System.Windows.Forms.DialogResult.Abort;
                 MessageBox.Show("Error al buscar hoteles.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
