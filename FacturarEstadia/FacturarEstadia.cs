b87df523254b4bb013df7aa164acfc1efda8c749
Nicolas Liloia Scott <liloiascott@gmail.com>
2018-07-01 15:13:01 -0300

    cambio en duplicacion de habitaciones

--- FacturarEstadia/FacturarEstadia.cs
+++ FacturarEstadia/FacturarEstadia.cs
@@ -36,100 +36,105 @@ namespace FrbaHotel.FacturarEstadia
                 for (int i = 0; i < nroHabitaciones.Length; i++)
                 {
                     nroHabitaciones[i] = nroHabitaciones[i].Trim();
-                }*/
+            }*/
 
-                idEstadias.Clear();
-                idReservaHabitaciones.Clear();
-                nroHabitacionValidas.Clear();
-                int temp = 0;
+            idEstadias.Clear();
+            idReservaHabitaciones.Clear();
+            nroHabitacionValidas.Clear();
+            int temp = 0;
 
-                //foreach (var nroHabitacion in nroHabitaciones)
-                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            List<string> nroHabitaciones = new List<string>();
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                nroHabitaciones.Add(dataGridView1.Rows[i].Cells[0].Value.ToString());
+            }
+            foreach (var nroHabitacion in nroHabitaciones.Distinct())
+            //for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                //string nroHabitacion = dataGridView1.Rows[i].Cells[0].Value.ToString();
+
+                var idEstadia = new SqlParameter("@idEstadia", SqlDbType.Int) { Direction = ParameterDirection.Output };
+                var idReservaHabitacion = new SqlParameter("@idReservaHabitacion", SqlDbType.Int) { Direction = ParameterDirection.Output };
+                int result = 0;
+
+                try
                 {
-                    string nroHabitacion = dataGridView1.Rows[i].Cells[0].Value.ToString();
-
-                    var idEstadia = new SqlParameter("@idEstadia", SqlDbType.Int) { Direction = ParameterDirection.Output };
-                    var idReservaHabitacion = new SqlParameter("@idReservaHabitacion", SqlDbType.Int) { Direction = ParameterDirection.Output };
-                    int result = 0;
-                    
-                    try
-                    {
-                        result = DBHandler.SPWithValue("MATOTA.GetEstadiaForHabitacion",
-                            new List<SqlParameter> {
-                            new SqlParameter("@nroHabitacion", nroHabitacion) { Direction = ParameterDirection.Input },
-                            new SqlParameter("@idHotel", Login.Login.LoggedUserSessionHotelID) { Direction = ParameterDirection.Input },
-                            new SqlParameter("@fechaSistema", ConfigManager.FechaSistema.ToString("yyyy-MM-dd") ) { Direction = ParameterDirection.Input },
-                            idEstadia,
-                            idReservaHabitacion
-                        }
-                        );
-
-                        if (result == -1)
-                        {
-                            MessageBox.Show("La estadia (id: "+idEstadia.Value.ToString()+") con fecha de ingreso mas reciente a la fecha del sistema sin fecha de salida definida para las habitaciones seleccionadas ya tiene una factura generada.\n\nEs decir, no hay estadia activa sin cerrar para las habitaciones seleccionadas.\n\nIntente modificar la fecha del sistema a una fecha mas tardia.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                            return;
-                        }
-                    }
-                    catch (Exception)
-                    {
-
-                        MessageBox.Show("Imposible cerrar la habitación " + nroHabitacion + ". Ocurrió un error inesperado.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        continue;
-                    }
-
-                    if (string.IsNullOrEmpty(idEstadia.ToString()) || string.IsNullOrEmpty(idReservaHabitacion.ToString()))
-                    {
-                        MessageBox.Show("Imposible cerrar la habitación " + nroHabitacion + ". La habitación no existe en el hotel o está mal escrito.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        continue;
-                    }
-                    else
-                    {
-                        idEstadias.Add(idEstadia.Value.ToString());
-                        idReservaHabitaciones.Add(idReservaHabitacion.Value.ToString());
-                        nroHabitacionValidas.Add(nroHabitacion);
-                        temp++;
-                    }
+                    result = DBHandler.SPWithValue("MATOTA.GetEstadiaForHabitacion",
+                        new List<SqlParameter> {
+                    new SqlParameter("@nroHabitacion", nroHabitacion) { Direction = ParameterDirection.Input },
+                    new SqlParameter("@idHotel", Login.Login.LoggedUserSessionHotelID) { Direction = ParameterDirection.Input },
+                    new SqlParameter("@fechaSistema", ConfigManager.FechaSistema.ToString("yyyy-MM-dd") ) { Direction = ParameterDirection.Input },
+                    idEstadia,
+                    idReservaHabitacion
                 }
+                    );
 
-                if (verificarEstadia(idEstadias) != 0)
-                {
-                    MessageBox.Show("Las habitaciones ingresadas no son de una única estadía.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-                else
-                {
-                    var query = new QueryBuilder(QueryBuilder.QueryBuilderType.SELECT)
-                    .Fields("reg.nombre")
-                    .Table("MATOTA.Regimen reg")
-                    .AddJoin("JOIN MATOTA.Reserva r ON (reg.idRegimen = r.idRegimen)")
-                    .AddJoin("JOIN MATOTA.Estadia e ON (r.idReserva = e.idReserva)")
-                    .AddEquals("e.idEstadia", idEstadias[0]);
-
-                    try
+                    if (result == -1)
                     {
-                        bool estadia = false;
-                        var nombreRegimen = DBHandler.Query(query.Build()).First()["nombre"].ToString();
-                        if (nombreRegimen == "All inclusive")
-                        {
-                            estadia = true;
-                        }
-                        else
-                        {
-                            estadia = false;
-                        }
-                        for (int i = 0; i < idEstadias.Count; i++)
-                        {
-                            new RegistrarConsumible.Registrar(idReservaHabitaciones[i], nroHabitacionValidas[i], estadia).ShowDialog(this);
-                        }
-                        new GenerarFactura(idEstadias[0], idReservaHabitaciones).ShowDialog(this);
-                        this.Close();
-                    }
-                    catch (Exception)
-                    {
-                        MessageBox.Show("Ocurrió un error al intentar obtener el régimen de la estadía.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show("La estadia (id: " + idEstadia.Value.ToString() + ") con fecha de ingreso mas reciente a la fecha del sistema sin fecha de salida definida para las habitaciones seleccionadas ya tiene una factura generada.\n\nEs decir, no hay estadia activa sin cerrar para las habitaciones seleccionadas.\n\nIntente modificar la fecha del sistema a una fecha mas tardia.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         return;
                     }
                 }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Imposible cerrar la habitación " + nroHabitacion + ". Ocurrió un error inesperado.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    continue;
+                }
+
+                if (string.IsNullOrEmpty(idEstadia.ToString()) || string.IsNullOrEmpty(idReservaHabitacion.ToString()))
+                {
+                    MessageBox.Show("Imposible cerrar la habitación " + nroHabitacion + ". La habitación no existe en el hotel o está mal escrito.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    continue;
+                }
+                else
+                {
+                    idEstadias.Add(idEstadia.Value.ToString());
+                    idReservaHabitaciones.Add(idReservaHabitacion.Value.ToString());
+                    nroHabitacionValidas.Add(nroHabitacion);
+                    temp++;
+                }
+            }
+
+            if (verificarEstadia(idEstadias) != 0)
+            {
+                MessageBox.Show("Las habitaciones ingresadas no son de una única estadía.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                var query = new QueryBuilder(QueryBuilder.QueryBuilderType.SELECT)
+                .Fields("reg.nombre")
+                .Table("MATOTA.Regimen reg")
+                .AddJoin("JOIN MATOTA.Reserva r ON (reg.idRegimen = r.idRegimen)")
+                .AddJoin("JOIN MATOTA.Estadia e ON (r.idReserva = e.idReserva)")
+                .AddEquals("e.idEstadia", idEstadias[0]);
+
+                try
+                {
+                    bool estadia = false;
+                    var nombreRegimen = DBHandler.Query(query.Build()).First()["nombre"].ToString();
+                    if (nombreRegimen == "All inclusive")
+                    {
+                        estadia = true;
+                    }
+                    else
+                    {
+                        estadia = false;
+                    }
+                    for (int i = 0; i < idEstadias.Count; i++)
+                    {
+                        new RegistrarConsumible.Registrar(idReservaHabitaciones[i], nroHabitacionValidas[i], estadia).ShowDialog(this);
+                    }
+                    new GenerarFactura(idEstadias[0], idReservaHabitaciones).ShowDialog(this);
+                    this.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Ocurrió un error al intentar obtener el régimen de la estadía.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
         }
 
         private void FacturarEstadia_Load(object sender, EventArgs e)
